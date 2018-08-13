2ab63d6da9b533b95d217ae59638e852025787ca
Kylor Hall <kylor@sharesight.com>
2018-08-13 22:14:14 +1200

    Init with some boilerplate / config.

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  plugins: [
+    'standard',
+    'react',
+  ],
+  parserOptions: {
+    ecmaVersion: 7,
+    ecmaFeatures: {
+      impliedStrict: true,
+      classes: true,
+      jsx: true,
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'import/no-unresolved': 0, // not compatible with parcel ~/ resolves
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    semi: ['error', 'always'],
+
+    // because eslint v5 + airbnb isn't so nice right now..
+    'react/destructuring-assignment': 'off',
+    'react/jsx-closing-tag-location': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-wrap-multilines': 'off',
+  },
+  settings: {
+    'import/extensions': [
+      '.js',
+      '.jsx',
+    ],
+    'import/resolver': {
+      node: {
+        paths: [
+          path.resolve(__dirname, './src'),
+          path.resolve(__dirname, './assets'),
+          path.resolve(__dirname, './node_modules'),
+        ],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+}
