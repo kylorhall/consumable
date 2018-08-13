7e81bfe4dccc030d87af5e05a781a0289b2f06e0
Kylor Hall <kylor@sharesight.com>
2018-08-13 23:35:15 +1200

    Add a rough logout.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Firebase from '~/helpers/firebase';
 import { Context } from '~/composers/Auth';
+
+import Button from '~/components/Button';
+
+const signOut = async () => Firebase.auth.signOut();
 
 export default () => <Context.Consumer>
   {({ user }) => {
-    if (user) return <div>Welcome {user.displayName || user.uid}</div>;
+    if (user) {
+      return <div>
+      Welcome {user.displayName || user.uid}
+
+        <Button onClick={signOut}>Sign Out</Button>
+      </div>;
+    }
     return <Link to="/login">Please Login</Link>;
   }}
 </Context.Consumer>;
