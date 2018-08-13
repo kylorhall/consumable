6288b9d62df73cb05a2c12173665ce01527845ca
Kylor Hall <kylor@sharesight.com>
2018-08-13 22:14:34 +1200

    Styled variables.

---
+++ src/styled/colors.js
@@ -0,0 +1,11 @@
+export default {
+  whiteGrey: '#F1F7ED',
+  lightGrey: '#E8E2D6',
+  green: '#5FAD56',
+  yellow: '#F2C14E',
+  pear: '#DDD92A',
+  orange: '#F78154',
+  indigo: '#254254',
+  blue: '#094D92',
+  link: '#7DCFB6',
+};
---
+++ src/styled/dimensions.js
@@ -0,0 +1,5 @@
+export default {
+  borderRadius: '.25rem',
+  fontSize: '1rem',
+  padding: '1rem',
+};
