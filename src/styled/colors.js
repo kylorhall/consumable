6cadcac1ef04d25e17643902fd2d6cc5010036f9
Kylor Hall <kylor@sharesight.com>
2018-08-13 23:35:38 +1200

    Some variables.

--- src/styled/colors.js
+++ src/styled/colors.js
@@ -1,10 +1,12 @@
 export default {
   whiteGrey: '#F1F7ED',
   lightGrey: '#E8E2D6',
+  grey: '#ccc',
   green: '#5FAD56',
   yellow: '#F2C14E',
   pear: '#DDD92A',
   orange: '#F78154',
+  red: 'red',
   indigo: '#254254',
   blue: '#094D92',
   link: '#7DCFB6',
