5ac98c8ba9e9f09b82ea211cca1125751788653b
Kylor Hall <kylor@sharesight.com>
2018-08-20 23:23:53 +1200

    Cleanup input a bit; use dimension variable.

--- src/components/Form/Input/Input.jsx
+++ src/components/Form/Input/Input.jsx
@@ -3,27 +3,25 @@ import styled, { css } from 'styled-components';
 import colors from '~/styled/colors';
 import dimensions from '~/styled/dimensions';
 
-const height = 1.875;
-
 export default styled.input`
   background-color: ${colors.whiteGrey};
   background-image: none;
-  border: 0.125rem solid ${colors.grey};
+  border: 1px solid ${colors.grey};
   box-shadow: none;
   position: relative;
   transition: all 250ms, height 0ms, width 0ms;
   vertical-align: top;
   width: 100%;
 
-  border-radius: ${height / 2}rem;
-  height: ${height}rem;
-  line-height: ${height - 0.25}rem; /* height - border widths */
+  border-radius: ${dimensions.borderRadius};
+  height: ${dimensions.height};
+  line-height: ${dimensions.lineHeight}; /* height - border widths */
   max-width: 19rem;
   padding: 0 ${dimensions.padding};
 
   &:focus,
   &:focus-within {
-    border-color: ${colors.link};
+    border-color: ${colors.blue};
     outline: none;
     box-shadow: none;
   }
