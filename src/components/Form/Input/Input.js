1f74ffb4f4ba5726509ea8a8902bfaea690b4273
Kylor Hall <kylor@sharesight.com>
2018-08-21 23:25:09 +1200

    These are already passed via props.

--- src/components/Form/Input/Input.js
+++ src/components/Form/Input/Input.js
@@ -7,12 +7,6 @@ import Input from './Input.jsx';
 export default props => <Label {...props}>
   <Input
     {...props}
-
-    onChange={props.onChange}
     value={props.value || ''}
-
-    // style props
-    full={props.full}
-    error={props.error}
   />
 </Label>;
