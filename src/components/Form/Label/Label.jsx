27453174d30187deb3b96c79cf0847fd199d2c57
Kylor Hall <kylor@sharesight.com>
2018-08-26 18:52:38 +1200

    Remove label; switched to MaterialUI.

--- src/components/Form/Label/Label.jsx
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components';
-
-import colors from '~/styled/colors';
-
-export default styled.div`
-  margin-bottom: 5px;
-
-  ${({ error }) => error && css`color: ${colors.red};`}
-`;
