df2b2e2da024d8a640226b6c7e433318885fedd5
Kylor Hall <kylor@sharesight.com>
2018-08-26 18:53:01 +1200

    Cleanup; use new MaterialUI inputs.

--- src/components/Form/EnergySelect.js
+++ src/components/Form/EnergySelect.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import { units } from '~/helpers/energy';
-
 import Select from '~/components/Form/Select';
 
-const options = Object.keys(units).map(key => ({ value: units[key].abbr, label: `${units[key].name}s` }));
+const options = Object.keys(units).map(key => ({ value: units[key].abbr, label: `${units[key].abbr}` }));
 
-export default props => <Select
-  label={props.label || 'Energy Unit'}
-  options={props.options || options}
-  value={props.value}
-  name={props.name || 'energyUnit'}
-  onChange={props.onChange}
-/>;
+export const EnergySelect = props => <Select {...props} />;
+
+EnergySelect.defaultProps = {
+  options,
+};
+
+export default EnergySelect;
--- src/components/Form/WeightSelect.js
+++ src/components/Form/WeightSelect.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import { units } from '~/helpers/weight';
-
 import Select from '~/components/Form/Select';
 
-const options = Object.keys(units).map(key => ({ value: units[key].abbr, label: `${units[key].name}s` }));
+const options = Object.keys(units).map(key => ({ value: units[key].abbr, label: `${units[key].abbr}` }));
 
-export default props => <Select
-  label={props.label || 'Weight Unit'}
-  options={props.options || options}
-  value={props.value}
-  name={props.name || 'weightUnit'}
-  onChange={props.onChange}
-/>;
+export const WeightSelect = props => <Select {...props} />;
+
+WeightSelect.defaultProps = {
+  options,
+};
+
+export default WeightSelect;
